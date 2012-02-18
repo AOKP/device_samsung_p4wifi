20d918050bfc77b6e91424fa8866dd8be547453c
Roman Birg <romanbirg@gmail.com>
2012-02-18 02:23:56 -0600

    Revert "move props from vendor"
    This reverts commit b815a01072cefe18fb4402a809f88068da8378c5.
    
    Conflicts:
    
    	p4wifi.mk

--- p4wifi.mk
+++ p4wifi.mk
@@ -36,22 +36,12 @@ $(call inherit-product, device/samsung/p4-common/p4-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/p4wifi/p4wifi-vendor.mk)
 
-$(call inherit-product, vendor/aokp/products/common_tablet.mk)
-
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := aokp_p4wifi
-PRODUCT_BRAND := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := p4wifi
 PRODUCT_DEVICE := p4wifi
-PRODUCT_MODEL := GT-P7510
+PRODUCT_MODEL := p4wifi
+PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P7510 BUILD_ID=HTJ85B BUILD_FINGERPRINT=samsung/GT-P7510/GT-P7510:3.2/HTJ85B/UEKMM:user/release-keys PRIVATE_BUILD_DESC="GT-P7510-user 3.2 HTJ85B UEKMM release-keys"
-
-# Inherit common build.prop overrides
--include vendor/aokp/products/common_versions.mk
-
-# Copy maguro specific prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/aokp/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
